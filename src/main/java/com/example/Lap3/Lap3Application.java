aa66fea1f69fa0e02f6ed8377e03fadee6993674
DELL <trongphuc20021203@gmail.com>
2023-05-20 11:18:56 +0700

    Initial commit

---
+++ src/main/java/com/example/Lap3/Lap3Application.java
@@ -0,0 +1,13 @@
+package com.example.Lap3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lap3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lap3Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/Lap3/repository/IBookRepository.java
@@ -0,0 +1,10 @@
+package com.example.Lap3.repository;
+
+import com.example.Lap3.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IBookRepository extends JpaRepository<Book, Long> {
+
+}
